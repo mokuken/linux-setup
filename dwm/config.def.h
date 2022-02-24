2fa3756d85070087aa323b81e8bef838243fb543
MokuKen <91607111+MokuKen@users.noreply.github.com>
2022-02-24 23:10:34 +0800

    Update config.def.h

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -61,6 +61,8 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/usr/local/bin/st", "-c", cmd, NULL } }
 
 /* commands */
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *powermenu[] = { "/home/mokuken/scripts/powermenu.sh", NULL };
 
