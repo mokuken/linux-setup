84bd5b9ef70b3a6878c3d84739ac52901cb30643
MokuKen <91607111+MokuKen@users.noreply.github.com>
2022-02-27 14:17:40 +0800

    Update config.def.h

--- dwm-v2/config.def.h
+++ dwm-v2/config.def.h
@@ -13,7 +13,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "SourceCodePro-Bold:size=9" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#141414";
 static const char col_gray2[]       = "#808080";
 static const char col_gray3[]       = "#FFFFFF";
 static const char col_gray4[]       = "#000000";
